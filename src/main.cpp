cd96a6480d3ce7ea3e29378372bfa5a747b515df
Kegofpad1 <hall1483@stthomas.edu>
2021-05-13 11:23:48 -0500

    render text/images

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 
 #define WIDTH 800
 #define HEIGHT 600
 
+typedef struct AppData{
+    TTF_Font * font;
+    SDL_Texture *phrase;
+    SDL_Texture *folder;
+    SDL_Texture *exe;
+    SDL_Texture *image;
+    SDL_Texture *video;
+    SDL_Texture *code;
+    SDL_Texture *other;
+}AppData;
 
-void initialize(SDL_Renderer *renderer);
-void render(SDL_Renderer *renderer);
+void initialize(SDL_Renderer *renderer, AppData *data_ptr);
+void render(SDL_Renderer *renderer, AppData *data_ptr);
+void quit(AppData *data_ptr);
 
 int main(int argc, char **argv)
 {
@@ -17,6 +29,7 @@ int main(int argc, char **argv)
     // initializing SDL as Video
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
+    TTF_Init();
 
     // create window and renderer
     SDL_Renderer *renderer;
@@ -24,39 +37,106 @@ int main(int argc, char **argv)
     SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
 
     // initialize and perform rendering loop
-    initialize(renderer);
-    render(renderer);
+    AppData data;
+    initialize(renderer, &data);
+    render(renderer, &data);
     SDL_Event event;
     SDL_WaitEvent(&event);
     while (event.type != SDL_QUIT)
     {
         //render(renderer);
         SDL_WaitEvent(&event);
+
+        render(renderer, &data);
     }
 
     // clean up
+    quit(&data);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit();
     IMG_Quit();
     SDL_Quit();
 
     return 0;
 }
 
-void initialize(SDL_Renderer *renderer)
+void initialize(SDL_Renderer *renderer, AppData *data_ptr)
 {
-    // set color of background when erasing frame
-    SDL_SetRenderDrawColor(renderer, 235, 235, 235, 255);
+    //Fill in the data for our App_data struct
+    data_ptr->font = TTF_OpenFont("resrc/OpenSans-Regular.ttf", 24);
+
+    SDL_Surface * img_surf = IMG_Load("resrc/images/folder.png");
+    data_ptr->folder = SDL_CreateTextureFromSurface(renderer, img_surf);
+    SDL_FreeSurface(img_surf);
+
+    img_surf = IMG_Load("resrc/images/exe.png");
+    data_ptr->exe = SDL_CreateTextureFromSurface(renderer, img_surf);
+    SDL_FreeSurface(img_surf);
+
+    img_surf = IMG_Load("resrc/images/code.png");
+    data_ptr->code = SDL_CreateTextureFromSurface(renderer, img_surf);
+    SDL_FreeSurface(img_surf);
+
+    img_surf = IMG_Load("resrc/images/image.png");
+    data_ptr->image = SDL_CreateTextureFromSurface(renderer, img_surf);
+    SDL_FreeSurface(img_surf);
+
+    img_surf = IMG_Load("resrc/images/other.png");
+    data_ptr->other = SDL_CreateTextureFromSurface(renderer, img_surf);
+    SDL_FreeSurface(img_surf);
+
+    img_surf = IMG_Load("resrc/images/video.png");
+    data_ptr->video = SDL_CreateTextureFromSurface(renderer, img_surf);
+    SDL_FreeSurface(img_surf);
+
+    SDL_Color color = {0,0,0,};
+    SDL_Surface *phrase_surf = TTF_RenderText_Solid(data_ptr->font, "TEMP",
+    color);
+
+    data_ptr->phrase = SDL_CreateTextureFromSurface(renderer, phrase_surf);
+    SDL_FreeSurface(phrase_surf); 
+    
+
 }
 
-void render(SDL_Renderer *renderer)
+void render(SDL_Renderer *renderer, AppData *data_ptr) 
 {
     // erase renderer content
+    SDL_SetRenderDrawColor(renderer, 235, 235, 235, 255);
     SDL_RenderClear(renderer);
     
     // TODO: draw!
+    //actually draw images/text(set size, position, ect.)
+    SDL_Rect rect;
+    rect.x = 200;
+    rect.y = 100;
+    rect.w = 165;
+    rect.h = 200;
+    SDL_RenderCopy(renderer, data_ptr->folder, NULL, &rect);
+
+    rect.x = 400;
+    rect.y = 300;
+    SDL_RenderCopy(renderer, data_ptr->exe, NULL, &rect);
+
+    SDL_QueryTexture(data_ptr->phrase, NULL, NULL, &(rect.w), &(rect.h));
+    rect.x = 10;
+    rect.y = 500;
+    SDL_RenderCopy(renderer, data_ptr->phrase, NULL, &rect);
+
 
     // show rendered frame
     SDL_RenderPresent(renderer);
+}
+
+void quit(AppData *data_ptr)
+{
+    SDL_DestroyTexture(data_ptr->folder);
+    SDL_DestroyTexture(data_ptr->exe);
+    SDL_DestroyTexture(data_ptr->image);
+    SDL_DestroyTexture(data_ptr->video);
+    SDL_DestroyTexture(data_ptr->code);
+    SDL_DestroyTexture(data_ptr->other);
+    TTF_CloseFont(data_ptr->font);
 }
 
